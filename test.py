ac5e999919f9e1a09075c6209ff8c794d03a113f
Serkan KINDAP <serkankindap@gmail.com>
2018-08-26 17:37:56 +0300

    test.py içinde değişiklik

---
+++ test.py
@@ -0,0 +1,7 @@
+articles = {"Title":"Deneme", "Body": "Deneme 123", "Author":"Serkan Kındap"}
+
+for x, y in articles.keys(), articles.values():
+    print(x ,y)
+
+def carpma(x, y):
+    return x * y
