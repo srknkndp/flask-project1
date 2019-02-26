43330005a3dbe51214043a591001a04ebe6bd6ea
Serkan KINDAP <serkankindap@gmail.com>
2019-02-26 23:59:23 +0300

    first commit flask project 1

--- test.py
+++
@@ -1,7 +0,0 @@
-articles = {"Title":"Deneme", "Body": "Deneme 123", "Author":"Serkan Kındap"}
-
-for x, y in articles.keys(), articles.values():
-    print(x ,y)
-
-def carpma(x, y):
-    return x * y
